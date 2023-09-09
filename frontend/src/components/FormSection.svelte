180a0b4ec00a21bdc60059157232c52ac88b35f6
Lauren Yim <cherryblossom0001234@gmail.com>
2023-09-09 21:07:41 +1000

    isArrString -> isNestedReq

--- frontend/src/components/FormSection.svelte
+++ frontend/src/components/FormSection.svelte
@@ -7,31 +7,40 @@
 
 	$: selected = undefined as undefined | string | CourseRequirement
 
-	const isArrString = (
-		x: CourseRequirement['requirement'],
-	): x is CourseRequirement['requirement'] & {items: string[]} =>
-		typeof x?.items[0] === 'string'
+	const isNestedReq = (
+		req: CourseRequirement['requirement'],
+	): req is CourseRequirement['requirement'] & {items: CourseRequirement[]} =>
+		typeof req?.items[0] === 'object'
 </script>
 
 <h2>{cur.title ?? ''}</h2>
 
 {#if cur.requirement?.operator === 'AND'}
-	{#if isArrString(cur.requirement)}
+	{#if isNestedReq(cur.requirement)}
+		{#each cur.requirement.items as next (next.title)}
+			<br />
+			<svelte:self cur={next} depth={depth + 1} />
+		{/each}
+	{:else}
 		<div class="flex flex-row flex-wrap">
 			{#each cur.requirement.items as next (next)}
 				<FormBox sel={next} selected={true} unit={true} />
 			{/each}
 		</div>
-	{:else}
-		{#each cur.requirement.items as next (next.title)}
-			<br />
-			<svelte:self cur={next} depth={depth + 1} />
-		{/each}
 	{/if}
 {:else if cur.requirement?.operator === 'OR'}
 	<div class="flex flex-row flex-wrap">
-		<!--Inside is units-->
-		{#if isArrString(cur.requirement)}
+		{#if isNestedReq(cur.requirement)}
+			<!--Inside is more requirements.-->
+			{#each cur.requirement.items as next (next.title)}
+				<FormBox
+					sel={next.title}
+					selected={next.title === selected}
+					onclick={() => (selected = next.title)}
+				/>
+			{/each}
+		{:else}
+			<!--Inside is units-->
 			{#each cur.requirement.items as next (next)}
 				<FormBox
 					unit={true}
@@ -40,20 +49,10 @@
 					onclick={() => (selected = next)}
 				/>
 			{/each}
-
-			<!--Inside is more requirements.-->
-		{:else}
-			{#each cur.requirement.items as next (next.title)}
-				<FormBox
-					sel={next.title}
-					selected={next.title === selected}
-					onclick={() => (selected = next.title)}
-				/>
-			{/each}
 		{/if}
 	</div>
 	{#if selected !== undefined}
-		{#if !isArrString(cur.requirement)}
+		{#if isNestedReq(cur.requirement)}
 			<svelte:self
 				depth={depth + 1}
 				cur={cur.requirement.items.filter(v => v.title === selected)[0]}
