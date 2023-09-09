3dbd704790957cf93b6a5529812823e94fe1005c
Lauren Yim <cherryblossom0001234@gmail.com>
2023-09-10 01:30:40 +1000

    ChooseNUnits

---
+++ frontend/src/components/ChooseNUnits.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import FormUnitBox from './FormUnitBox.svelte'
+	export let units: readonly string[]
+
+	export let selected: readonly string[] = []
+	export let count: number
+</script>
+
+{#each units as unit (unit)}
+	<FormUnitBox
+		unit={true}
+		disabled={selected.length === count && !selected.includes(unit)}
+		value={unit}
+		selected={selected.includes(unit)}
+		onclick={() =>
+			(selected = selected.includes(unit)
+				? selected.filter(x => x !== unit)
+				: [...selected, unit])}
+	/>
+{/each}
--- frontend/src/components/init/FormSection.svelte
+++ frontend/src/components/init/FormSection.svelte
@@ -1,16 +1,13 @@
 <script lang="ts">
+	import ChooseNUnits from '../ChooseNUnits.svelte'
 	import FormUnitBox from '../FormUnitBox.svelte'
+	import {isNestedReq} from '../../scripts'
 	import type {CourseRequirement} from 'shared'
 
 	export let cur: CourseRequirement
 	export let depth: number = 0
 
 	let selected: readonly string[] = []
-
-	const isNestedReq = (
-		req: CourseRequirement['requirement'],
-	): req is CourseRequirement['requirement'] & {items: CourseRequirement[]} =>
-		typeof req?.items[0] === 'object'
 </script>
 
 <svelte:element this={`h${Math.min(depth + 2, 6)}`}
@@ -31,73 +28,37 @@
 	{/if}
 {:else if cur.requirement.operator === 'OR'}
 	<div class="flex flex-row flex-wrap">
-		{#if isNestedReq(cur.requirement)}
-			<!--Inside is more requirements.-->
-			{#each cur.requirement.items as next (next.title)}
-				<FormUnitBox
-					value={next.title}
-					selected={next.title === selected[0]}
-					onclick={() => (selected = [next.title])}
-				/>
-			{/each}
-		{:else}
-			<!--Inside is units-->
-			{#each cur.requirement.items as next (next)}
-				<FormUnitBox
-					unit={true}
-					value={next}
-					selected={next === selected[0]}
-					onclick={() => (selected = [next])}
-				/>
-			{/each}
-		{/if}
-	</div>
-	{#if selected.length}
-		{#if isNestedReq(cur.requirement)}
-			<svelte:self
-				depth={depth + 1}
-				cur={cur.requirement.items.find(v => v.title === selected[0])}
+		{#each isNestedReq(cur.requirement) ? cur.requirement.items.map(r => r.title) : cur.requirement.items as next (next)}
+			<FormUnitBox
+				value={next}
+				selected={next === selected[0]}
+				onclick={() => (selected = [next])}
 			/>
-		{/if}
+		{/each}
+	</div>
+	{#if selected.length && isNestedReq(cur.requirement)}
+		<svelte:self
+			depth={depth + 1}
+			cur={cur.requirement.items.find(v => v.title === selected[0])}
+		/>
 	{/if}
 {:else}
 	<!-- pick 2 of the units -->
 	<div class="flex flex-row flex-wrap">
-		{#if isNestedReq(cur.requirement)}
-			<!--Inside is more requirements.-->
-			{#each cur.requirement.items as next (next.title)}
-				<FormUnitBox
-					value={next.title}
-					disabled={selected.length === 2 && !selected.includes(next.title)}
-					selected={selected.includes(next.title)}
-					onclick={() =>
-						(selected = selected.includes(next.title)
-							? selected.filter(x => x !== next.title)
-							: [...selected, next.title])}
-				/>
-			{/each}
-		{:else}
-			<!--Inside is units-->
-			{#each cur.requirement.items as next (next)}
-				<FormUnitBox
-					unit={true}
-					disabled={selected.length === 2 && !selected.includes(next)}
-					value={next}
-					selected={selected.includes(next)}
-					onclick={() =>
-						(selected = selected.includes(next)
-							? selected.filter(x => x !== next)
-							: [...selected, next])}
-				/>
-			{/each}
-		{/if}
+		<ChooseNUnits
+			bind:selected
+			count={2}
+			units={isNestedReq(cur.requirement)
+				? // Inside is more requirements
+				  cur.requirement.items.map(r => r.title)
+				: // Inside is units
+				  cur.requirement.items}
+		/>
 	</div>
-	{#if selected.length === 2}
-		{#if isNestedReq(cur.requirement)}
-			<!-- TODO: investigate wtf prettier is doing here? -->
-			{#each cur.requirement.items.filter( v => selected.includes(v.title), ) as next}
-				<svelte:self depth={depth + 1} cur={next} />
-			{/each}
-		{/if}
+	{#if selected.length === 2 && isNestedReq(cur.requirement)}
+		<!-- TODO: investigate wtf prettier is doing here? -->
+		{#each cur.requirement.items.filter( v => selected.includes(v.title), ) as next}
+			<svelte:self depth={depth + 1} cur={next} />
+		{/each}
 	{/if}
 {/if}
