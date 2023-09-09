56f623656739ad1cf4a0e249d7cd76b038302cb8
Lauren Yim <cherryblossom0001234@gmail.com>
2023-09-09 16:10:14 +1000

    remove build output

--- backend/api/unit.d.ts
+++
@@ -1,2 +0,0 @@
-declare const _default: (req: import("@vercel/node").VercelRequest, res: import("@vercel/node").VercelResponse) => import("@vercel/node").VercelResponse | undefined;
-export default _default;
